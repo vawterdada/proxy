c7f9862178e0b1171bb7bc9c37426a2fc943d3af
vawterdada <130421680+vawterdada@users.noreply.github.com>
2023-05-05 09:48:19 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://api.openai.com/v1/engines/davinci/completions";//your website url
+  let target = "";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
